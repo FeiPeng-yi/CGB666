d03ec96da472a92e2d28ca8dd2aeccb2419dbf7b
HTYPF2964 <2586283122@qq.com>
2019-11-22 10:57:46 +0800

    CGB66666

---
+++ CGB-DB-SYS-V8-2888/src/main/java/com/cy/CgbDbSysV82888Application.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgbDbSysV82888Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgbDbSysV82888Application.class, args);
+	}
+
+}
---
+++ CGB-DB-SYS-V8-2888/src/test/java/com/cy/CgbDbSysV82888ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgbDbSysV82888ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
